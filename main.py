d904d92262e9f6bf41d5592ee108cc890cb36a37
Tom Lockwood <71411266+XenioxYT@users.noreply.github.com>
2022-02-13 15:12:53 +0000

    merge

--- main.py
+++ main.py
@@ -3,18 +3,41 @@ from random import choice
 
 word_file = open("Words.txt","r")
 words = word_file.readlines()
+# allowedwords_file = open("Allowed_words.txt","r")
 
+#function to check the word against the 
+def check(word):
+    with open('Allowed_words.txt') as f:
+        datafile = f.readlines()
+    for line in datafile:
+        if word in line:
+            return True
+    return False  # Because you finished the search without finding
+
+
+#choose a random word
 answer=choice(words)
 answer=answer.upper()
 answer = answer.rstrip()
 word = ""
+wordcorrect = False
+print(answer) #remove after (used for debugging)
 i = 0
 while word != answer and i < 6:
 
+  #get the user to input a word
   word = input(Fore.WHITE + "Input a word: ")
-  if len(word) != 5:
+  word = word.upper() #make it uppercase
+  if len(word) != 5: #check the length of the word
     print("This word is invalid")
+    continue #try again basically
+  
+  #check the word against the allowed_words.txt file
+  if check(word.lower()) == False:
+    print("The word is not an acceptable word")
     continue
+
+  #check each stage of the word to check if it is valid or not
 
   if word[0] == answer[0]:
     print(Fore.GREEN + word[0], "is in the correct position")
@@ -50,7 +73,11 @@ while word != answer and i < 6:
     print(Fore.YELLOW + word[4], "is in the word but not in that position")
   else:
     print(Fore.RED + word[4], "is not in the word")
-  
+  #next i
   i+= 1
 
-print(Fore.WHITE + "CORRECT!")
+#final checks because the program would still output the word was correct
+if i > 6:
+  print(Fore.WHITE + "CORRECT!")
+else:
+  print(Fore.WHITE + "You didn't get the word in 6 guesses, try again!")
